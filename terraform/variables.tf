04e9a309f876ec46e97e77b62cc376ef2cecbaf5
sirishap03 <ponnadasirisha01@gmail.com>
2025-06-03 19:48:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,29 @@
 variable "project_id" {
+  description = "GCP project ID"
   type        = string
-  description = "The GCP project ID"
 }
 
 variable "region" {
-  type        = string
   description = "GCP region"
+  type        = string
   default     = "us-central1"
 }
 
 variable "zone" {
-  type        = string
   description = "GCP zone"
+  type        = string
   default     = "us-central1-a"
 }
+
+variable "ssh_user" {
+  description = "Username for SSH login"
+  type        = string
+  default     = "ansible"
+}
+
+variable "ssh_pub_key_path" {
+  description = "Path to the SSH public key"
+  type        = string
+  default     = "~/.ssh/ansible.pub"
+}
+
