202bd9bd57f691417dc1349da29e38ecca779182
sirishap03 <ponnadasirisha01@gmail.com>
2025-06-03 19:30:53 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  type        = string
+  description = "GCP zone"
+  default     = "us-central1-a"
+}
